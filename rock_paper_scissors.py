d3f0d6d391686709ad0385dbaf626bdc8de82b57
Jeff Campbell <jtcampbell399@gmail.com>
2025-04-16 21:10:44 -0500

    Version 3.0 - user can now use single letter to input choice

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,19 +1,19 @@
 # Rock, Paper, Scissors Game
-# v1 Basic Game
-# v2 Added Bonus Features Other Than Shortened Input
+# v1.0 Basic Game
+# v2.0 Added Bonus Features Other Than Shortened Input
+# v3.0 Added Shortened Input
 
 import os
 import random
 import json
 
-VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+VALID_CHOICES = ['r', 'p', 's', 'l', 'S']
 was_a_tie = True
 no_winner = True
 game_count = 0
 match_count = 0
 matches_won_by_player = 0
 matches_won_by_computer = 0
-number_of_ties = 0
 
 with open('rock_paper_scissors_messages.json', 'r') as file:
     MESSAGES = json.load(file)
@@ -25,7 +25,8 @@ def prompt(message):
     print(f"==> {message}")
 
 def boundary():
-    print("------------------------------------------------------------------------")
+    print("-------------------------------------------------"
+          "-----------------------")
 
 def display_welcome():
     clear_screen()
@@ -43,30 +44,24 @@ def calculate_winner_of_round(user_choice, computer_choice):
     global user_wins
     global computer_wins
     global game_count
-    global number_of_ties
 
     game_count += 1
 
-    if ((user_choice == 'rock' and (computer_choice == 'paper' or computer_choice == 'spock')) or 
-        (user_choice == 'paper' and (computer_choice == 'scissors' or computer_choice == 'lizard')) or 
-        (user_choice == 'scissors' and (computer_choice == 'spock' or computer_choice == 'rock')) or
-        (user_choice == 'spock' and (computer_choice == 'lizard' or computer_choice == 'paper')) or
-        (user_choice == 'lizard' and (computer_choice == 'rock' or computer_choice == 'scissors'))):
+    if user_choice == computer_choice:
+        was_a_tie = True
+        return "This round is a tie!"
+    elif((user_choice == 'r' and computer_choice in ['p', 'S']) or
+         (user_choice == 'p' and computer_choice in ['s', 'l']) or
+         (user_choice == 's' and computer_choice in ['S', 'r']) or
+         (user_choice == 'S' and computer_choice in ['l', 'p']) or
+         (user_choice == 'l' and computer_choice in ['r', 's'])):
         computer_wins += 1
         was_a_tie = False
         return "Computer wins this round."
-    elif((computer_choice == 'rock' and (user_choice == 'paper' or user_choice == 'spock')) or 
-        (computer_choice == 'paper' and (user_choice == 'scissors' or user_choice == 'lizard')) or 
-        (computer_choice == 'scissors' and (user_choice == 'spock' or user_choice == 'rock')) or
-        (computer_choice == 'spock' and (user_choice == 'lizard' or user_choice == 'paper')) or
-        (computer_choice == 'lizard' and (user_choice == 'rock' or user_choice == 'scissors'))):
+    else:
         user_wins += 1
         was_a_tie = False
         return f"{user_name} wins this round!"
-    else:
-        number_of_ties += 1
-        was_a_tie = True
-        return "This round is a tie!"
 
 def display_match_results(user_wins, computer_wins):
     global no_winner
@@ -80,14 +75,17 @@ def display_match_results(user_wins, computer_wins):
     print()
     boundary()
 
-    if user_wins >= 2:
-        print(MESSAGES['end_of_match']) 
-        print(f"{user_name} won the match against Computer by a score of {user_wins} to {computer_wins}!")
+    if user_wins >= 3:
+        print(MESSAGES['end_of_match'])
+        print(f"{user_name} won the match against Computer by "
+              f"a score of {user_wins} to {computer_wins}!")
         matches_won_by_player += 1
     else:
-        print(MESSAGES['end_of_match'] + f"Computer won the match, beating {user_name} by a score of {computer_wins} to {user_wins}. Bummer.")
+        print(MESSAGES['end_of_match'] + f"Computer won the match, "
+              f"beating {user_name} by a score of {computer_wins} "
+              f"to {user_wins}. Bummer.")
         matches_won_by_computer += 1
-    
+
     boundary()
     print()
 
@@ -97,34 +95,43 @@ def display_final_results():
     boundary()
 
     if match_count == 1:
-        print(f"Thank you for playing one match (consisting of {game_count} games) of "
-              "Rock, Paper, \nScissors, Lizard, Spock!\n")
+        print(f"Thank you for playing one match (consisting of "
+              f"{game_count} games) of Rock, Paper, \n"
+              "Scissors, Lizard, Spock!\n")
     else:
-        print(f"Thank you for playing a total of {match_count} matches (consisting of "
-              f"{game_count} games) of \nRock, Paper, Scissors, Lizard, Spock!\n")
-    
+        print(f"Thank you for playing a total of {match_count} "
+              f"matches (consisting of {game_count} games) of \n"
+              "Rock, Paper, Scissors, Lizard, Spock!\n")
+
     if matches_won_by_player == 0:
         if matches_won_by_computer == 1:
-            print("Pathetic. You lost the only match you played against the computer."
+            print("Pathetic. You lost the only match you played "
+                  "against the computer."
                   "\nWhere is your human pride?")
         else:
-            print(f"Unfortunately, you didn't win a single match, and the computer blanked"
-                  f"\nyou {matches_won_by_computer} to zero. Ouch.")
+            print(f"Unfortunately, you didn't win a single match, "
+                  f"and the computer blanked \nyou {matches_won_by_computer}"
+                   "to zero. Ouch.")
     elif matches_won_by_computer == 0:
         if matches_won_by_player == 1:
-            print("You won the only match you played against the computer. Ho hum.")
+            print("You won the only match you played against the "
+                  "computer. Ho hum.")
         else:
-            print(f"You thrashed your digital overlord by winning all {matches_won_by_player} "
-                   "matches you played. \nCongrats, but watch your back!")
+            print("You thrashed your digital overlord by winning "
+                  f"all {matches_won_by_player} matches you played. \n"
+                  "Congrats, but watch your back!")
     elif matches_won_by_player > matches_won_by_computer:
-        print(f"You dominated, crushing the puny digital brain by {matches_won_by_player} to "
+        print("You dominated, crushing the puny digital brain by "
+              f"{matches_won_by_player} to "
               f"{matches_won_by_computer}. Go humans!")
     elif matches_won_by_computer > matches_won_by_player:
-        print(f"Are you sure you're actually human? Losing by {matches_won_by_computer} to "
-              f"{matches_won_by_player} is... sad, and might \nmean that you have more homo erectus"
+        print(f"Are you sure you're actually human? Losing by "
+              f"{matches_won_by_computer} to {matches_won_by_player} "
+              "is... sad, and might \nmean that you have more homo erectus "
               "DNA than homo sapiens DNA.")
     else:
-        print(f"The match was tied {matches_won_by_player} to {matches_won_by_computer}. "
+        print(f"The match was tied {matches_won_by_player} to "
+              f"{matches_won_by_computer}. "
               "Very unsatisfying result.")
     print()
     print(f"{user_name}, " + MESSAGES['final_message'])
@@ -142,8 +149,7 @@ while True:
 
     if len(user_name) > 0 and len(user_name) < 20 and user_name[0] != " ":
         break
-    else:
-        prompt(MESSAGES['user_name_error'])
+    prompt(MESSAGES['user_name_error'])
 
 while True:
     prompt(MESSAGES['ready_to_play'])
@@ -152,8 +158,7 @@ while True:
     if user_answer[0] == 'y':
         clear_screen()
         break
-    else:
-        prompt(MESSAGES['wrong_choice_to_start_game'])
+    prompt(MESSAGES['wrong_choice_to_start_game'])
 
 # Main Game Loop
 while True:
@@ -161,27 +166,29 @@ while True:
     computer_wins = 0
 
     while no_winner:
-    
         # Obtain User Input
-        prompt(f"Choose one: {", ".join(VALID_CHOICES)}")
-        user_choice = input().lower()
+        # prompt(f"Enter your choice: {", ".join(VALID_CHOICES)}")
+        prompt("Enter your choice (r, p, s, l, S): ") # Temporary for Pylint
+        user_choice = input()
         print()
 
         while user_choice not in VALID_CHOICES:
             prompt(MESSAGES['invalid_choice'])
-            user_choice = input().lower()
+            user_choice = input()
 
         # Obtain Computer Input
         computer_choice = random.choice(VALID_CHOICES)
 
         # Calculate and Display Results
-        prompt(f"{user_name} chose {user_choice}, computer chose {computer_choice}...")
-        prompt(calculate_winner_of_round(user_choice, computer_choice))
+        prompt(f"{user_name} chose {user_choice}, computer "
+               f"chose {computer_choice}...")
+        prompt(calculate_winner_of_round(user_choice,
+                                         computer_choice))
 
         if computer_wins < 3 and user_wins < 3:
-            prompt(f"Currently, the score is {user_name}: {user_wins}, Computer: {computer_wins}\n")
-
-        if computer_wins >= 3 or user_wins >= 3:
+            prompt(f"Currently, the score is {user_name}: "
+                   f"{user_wins}, Computer: {computer_wins}\n")
+        else:
             display_match_results(user_wins, computer_wins)
 
     # Play Again?
@@ -193,9 +200,8 @@ while True:
             no_winner = True
             clear_screen()
             break
-        else:
-            prompt(MESSAGES['another_invalid_choice'])
-        
+        prompt(MESSAGES['another_invalid_choice'])
+
     if play_again[0] == 'n':
         clear_screen()
         display_final_results()
